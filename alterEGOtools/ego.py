e346561c926505b8dfa5b218a298557b2d1f8071
Pascal Malouin <pascal.malouin@gmail.com>
2021-06-08 06:19:33 -0400

    Up to arch-chroot

--- alterEGOtools/ego.py
+++ alterEGOtools/ego.py
@@ -11,6 +11,12 @@ import argparse
 import os
 import subprocess
 
+git_tools = 'https://github.com/fantomH/alterEGOtools.git'
+git_alterEGO = 'https://github.com/fantomH/alterEGO.git'
+usr_local = '/usr/local'
+local_tools = f'{usr_local}/alterEGOtools'
+local_alterEGO f'{usr_local}/alterEGO'
+
 def create_partition():
     partition = '''label: dos
                    device: /dev/sda
@@ -43,6 +49,7 @@ def create_partition():
                'linux',
                'networkmanager',
                'pacman-contrib',
+               'python',
                'vim']
     
     subprocess.run(['pacstrap', '/mnt'], input=' '.join(min_pkg), text=True)
@@ -51,16 +58,30 @@ def create_partition():
 
     subprocess.run('genfstab -U /mnt >> /mnt/etc/fstab', shell=True)
 
+def chroot():
+    '''
+    Preparing and changing the root to the new system.
+    '''
+    
+    subprocess.run(['git', 'clone', git_tools, '/mnt/usr/local'])
+    subprocess.run(['arch-chroot', '/mnt', 'python', '/usr/local/alterEGOtools/ego.py', '--sysconfig'])
+
+def sysconfig():
+    subprocess.run(['git', 'clone', git_alterEGO, usr_local])
+    
 def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--minimal", action="store_true", help="Install a minimal instance of Arch Linux.")
+    parser.add_argument("--sysconfig", action="store_true", help="Stage 2")
 
     args = parser.parse_args()
 
     if args.minimal:
-        create_partition()
-        # print('hello')
+        # create_partition()
+        chroot()
+    if args.sysconfig:
+        sysconfig()
 
 if __name__ == '__main__':
     main()
