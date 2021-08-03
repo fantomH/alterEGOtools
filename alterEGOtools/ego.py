9b3782467155b01c1488fb529995629782d2a523
Pascal Malouin <pascal.malouin@gmail.com>
2021-08-03 18:34:16 -0400

    Fix sddm copy directory.

--- alterEGOtools/ego.py
+++ alterEGOtools/ego.py
@@ -447,7 +447,7 @@ def sysconfig(mode):
 
     if mode == 'beast':
         shutil.copy(os.path.join(localEGO, 'global', 'etc', 'sddm.conf'), '/etc/sddm.conf')
-        copy_recursive(os.path.join(localEGO, 'global', 'usr', 'share', 'sddm', 'themes', 'alterEGO-simplyblack'), '/usr/share/sddm/themes/alterEGO-simplyblack')
+        copy_recursive(os.path.join(localEGO, 'global', 'usr', 'share', 'sddm'), '/usr/share/')
 
     #-----[ GENERATING mandb ]
 
