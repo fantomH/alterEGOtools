d24bbbaafe19063037710d7747d1563e15fa5e32
Pascal Malouin <pascal.malouin@gmail.com>
2021-08-03 16:15:21 -0400

    ...

--- alterEGOtools/ego.py
+++ alterEGOtools/ego.py
@@ -461,8 +461,7 @@ def sysconfig(mode):
     if mode == 'beast':
         shutil.copy(os.path.join(localEGO, 'global', 'etc', 'sddm.conf'),
                     '/etc/sddm.conf')
-        # copy_recursive(os.path.join(localEGO, 'global', 'usr', 'share', 'sddm', 'themes', 'alterEGO-simplyblack'),
-                        # '/usr/share/sddm/themes/alterEGO-simplyblack')
+        # copy_recursive(os.path.join(localEGO, 'global', 'usr', 'share', 'sddm', 'themes', 'alterEGO-simplyblack'), '/usr/share/sddm/themes/alterEGO-simplyblack')
 
     #-----[ GENERATING mandb ]
 
