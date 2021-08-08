39e02df9a141afb5762555d91ca855ee7e39f69f
Pascal Malouin <pascal.malouin@gmail.com>
2021-08-08 08:47:14 -0400

    ...

--- alterEGOtools/ego.py
+++ alterEGOtools/ego.py
@@ -169,23 +169,26 @@ def copy_recursive(src, dst):
     if not os.path.exists(dst):
         os.makedirs(dst)
 
-    msg(f":: Copying files to {dst}...", color='green')
+    Msg.console(f":: {_green}Copying files to {dst}...", wait=5)
 
     for src_dir, dirs, files in os.walk(src):
         dst_dir = src_dir.replace(src, dst)
         if not os.path.exists(dst_dir):
             os.mkdir(dst_dir)
+            Msg.console(f" -> {_blue}Creating directory {dst_dir}...", wait=1)
 
         for f in files:
             src_file = os.path.join(src_dir, f)
             dst_file = os.path.join(dst_dir, f)
-            print(f" -> Copying {dst_file}")
+            Msg.console(f" -> {_blue}Copying {dst_file}...", wait=1)
 
             if os.path.exists(dst_file):
                 os.remove(dst_file)
             shutil.copy2(src_file, dst_file)
 
 def execute(cmd, cwd=None):
+    # .. TODO: Send to logs.
+
     cmd_list = shlex.split(cmd)
     cmd_run = subprocess.run(cmd_list, cwd=cwd)
 
@@ -199,18 +202,63 @@ def git(git_repository, local_directory):
     else:
         execute(f"git -C {local_directory} pull")
 
-def msg(message, color=None):
-    colors = {
-        'green': '\033[92m'
-            }
-    reset = '\033[00m'
-    if color is not None:
-        msg_color = colors.get(color)
-        print(f"\n{msg_color}{message}{reset}")
-    sleep(5)
+class Msg:
 
-def testrerun(string):
-    print(string)
+    def color(color='white'):
+        colors = {
+            'reset': '\033[00m',
+            'bold': '\033[1m',
+            'underline': '\033[4m',
+            'dim': '\033[2m',
+            'strickthrough': '\033[9m',
+            'blink': '\033[5m',
+            'reverse': '\033[7m',
+            'hidden': '\033[8m',
+            'normal': '\033[0m',
+            'black': '\033[30m',
+            'red': '\033[31m',
+            'green': '\033[32m',
+            'orange': '\033[33m',
+            'blue': '\033[34m',
+            'purple': '\033[35m',
+            'aqua': '\033[36m',
+            'gray': '\033[37m',
+            'darkgray': '\033[90m',
+            'lightred': '\033[91m',
+            'lightgreen': '\033[92m',
+            'lightyellow': '\033[93m',
+            'lightblue': '\033[94m',
+            'lightpurple': '\033[95m',
+            'lightaqua': '\033[96m',
+            'white': '\033[97m',
+            'default': '\033[39m',
+            'bgblack': '\033[40m',
+            'bgred': '\033[41m',
+            'bggreen': '\033[42m',
+            'bgorange': '\033[43m',
+            'bgblue': '\033[44m',
+            'bgpurple': '\033[45m',
+            'bgaqua': '\033[46m',
+            'bggray': '\033[47m',
+            'bgdarkgray': '\033[100m',
+            'bglightred': '\033[101m',
+            'bglightgreen': '\033[102m',
+            'bglightyellow': '\033[103m',
+            'bglightblue': '\033[104m',
+            'bglightpurple': '\033[105m',
+            'bglightaqua': '\033[106m',
+            'bgwhite': '\033[107m',
+            'bgdefault': '\033[49m',
+            }
+
+        return colors.get(color)
+
+    def console(message, wait=0):
+        print(message + Msg.color('reset'))
+        time.sleep(wait)
+
+_blue = Msg.color('blue')
+_green = Msg.color('lightgreen')
 
 # { INSTALLER FUNCTIONS }______________________________________________________
 
@@ -229,47 +277,48 @@ def packages(required_by, mode=None):
     return pkgs_list
 
 def shared_resources():
+    # -- TODO: All file copying should be one functions.
+    # .. recursive, copy, symlinks...
 
-    #-----[ bookmarks.db ]
+    # [ bookmarks.db ]_________________________________________________________
     f = 'bookmarks.db'
-    print(f":: Deploying {f} to /usr/local/share...")
+    Msg.console(f":: {_green}Deploying {f} to /usr/local/share...", wait=5)
     src = os.path.join(localEGO, 'share', f)
     dst = os.path.join('/usr/local/share', f)
     os.symlink(src, dst)
 
 def shared_bin():
+    # -- Deploys applications.
 
-    #### Deploys applications.
-
+    Msg.console(f":: {_green}Deploying application to /usr/local/bin...", wait=5)
     localEGO_bin = f"{localEGO}/bin"
     files = os.listdir(localEGO_bin)
 
     for f in files:
-        print(f":: Deploying {f} to /usr/local/bin...")
+        Msg.console(f" -> {_blue}{f}", wait=1)
         src = os.path.join(localEGO_bin, f)
         dst = os.path.join('/usr/local/bin', f)
         os.symlink(src, dst)
 
 def shared_wordlist():
+    # -- Deploys wordlists.
 
-    #### Deploys wordlists.
-
+    Msg.console(f":: {_green}Deploying application to /usr/local/share/wordlist...", wait=5)
     if not os.path.exists('/usr/local/share/wordlist'):
         os.mkdir('/usr/local/share/wordlist')
 
     localEGO_wordlist = f"{localEGO}/share/wordlist"
     files = os.listdir(localEGO_wordlist)
 
-    for f in files:
-        print(f":: Deploying {f} to /usr/local/share/wordlist...")
+        Msg.console(f" -> {_blue}{f}", wait=1)
         src = os.path.join(localEGO_wordlist, f)
         dst = os.path.join('/usr/local/share/wordlist', f)
         os.symlink(src, dst)
 
 def shared_reverse_shell():
+    # -- Deploys reverse shells.
 
-    #### Deploys reverse shells.
-
+    Msg.console(f":: {_green}Deploying application to /usr/local/share/reverse_shell...", wait=5)
     if not os.path.exists('/usr/local/share/reverse_shell'):
         os.mkdir('/usr/local/share/reverse_shell')
 
@@ -277,19 +326,22 @@ def shared_reverse_shell():
     files = os.listdir(localEGO_reverse_shell)
 
     for f in files:
-        print(f":: Deploying {f} to /usr/local/share/reverse_shell...")
+        Msg.console(f" -> {_blue}{f}", wait=1)
         src = os.path.join(localEGO_reverse_shell, f)
         dst = os.path.join('/usr/local/share/reverse_shell', f)
         os.symlink(src, dst)
 
 def pacman(mode):
+    Msg.console(f":: {_green}Starting pacman...", wait=5)
     pkgs_list = ' '.join(packages('pacman', mode))
-    msg(f'Packages list: \n{pkgs_list}', color='green')
+    Msg.console(f" -> {_blue}Will install:\n{pkgs_list}", wait=1)
     execute(f"pacman -Syu --noconfirm --needed {pkgs_list}")
 
 def pacstrap():
 
+    Msg.console(f":: {_green}Starting pacstrap...", wait=5)
     pkgs_list = ' '.join(packages('pacstrap'))
+    Msg.console(f" -> {_blue}Will install:\n{pkgs_list}", wait=1)
 
     try:
         execute(f"pacstrap /mnt {pkgs_list}")
@@ -312,6 +364,7 @@ def pacstrap():
 
 def swapfile():
 
+    Msg.console(f"{_green}Creating a 1G swapfile...", wait=5)
     execute(f"fallocate -l 1G /swapfile")
     os.chmod('/swapfile', 0o600)
     execute(f"mkswap /swapfile")
@@ -322,7 +375,8 @@ def swapfile():
 # { INSTALLER }________________________________________________________________
 
 def installer(mode):
-    msg(':: Creating the partition...', color='green')
+    # [ PARTITION ]
+    Msg.console(f":: {_green}Creating and mounting the partition...", wait=5)
     partition = '''label: dos
                    device: /dev/sda
                    unit: sectors
@@ -333,64 +387,79 @@ def installer(mode):
 
     subprocess.run(['sfdisk', '/dev/sda'], text=True, input=partition)
 
-    #### Formating the File System.
+    # -- Formating the File System.
 
     execute(f"mkfs.ext4 /dev/sda1")
 
-    #### Mounting /dev/sda1 to /mnt.
+    # -- Mounting /dev/sda1 to /mnt.
 
     execute(f"mount /dev/sda1 /mnt")
 
-    #### Creating ${HOME}. 
+    # -- Creating ${HOME}. 
 
     os.mkdir('/mnt/home')
-    print(f"  -> Created {os.listdir('/mnt')}")
 
-    # [ PACSTRAP ]_____________________________________________________________
+    Msg.console(f" -> {_blue}Created {os.listdir('/mnt')}", wait=1)
+
+    # [ PACSTRAP ]
 
     pacstrap()
 
-    #### Generating the fstab.
+    # [ FSTAB ]
+    Msg.console(f":: {_green}Generating the fstab...", wait=1)
     subprocess.run('genfstab -U /mnt >> /mnt/etc/fstab', shell=True)
 
+    # [ ARCH-ROOT ]
+    Msg.console(f":: {_green}Preparing arch-root...", wait=10)
     shutil.copy('/root/ego.py', '/mnt/root/ego.py')
     if mode == 'minimal':
         execute(f'arch-chroot /mnt python /root/ego.py --sysconfig minimal')
     elif mode == 'beast':
         execute(f'arch-chroot /mnt python /root/ego.py --sysconfig beast')
 
-    # execute(f'umount -R /mnt')
-    # execute(f'shutdown now') 
+    # [ ALL DONE ]
+    all_done = input(f":: {_green}Shutdown [Y/n]? ")
+    if all_done.lower() in ['y', 'yes']:
+        Msg.console{f" -> {_blue}Good Bye!", wait=10}
+        try:
+            execute(f'umount -R /mnt')
+            execute(f'shutdown now') 
+        except:
+            execute(f'shutdown now') 
+    else:
+        Msg.console(f" -> {_blue}Do a manual shutdown when ready.", wait=10)
 
 def sysconfig(mode):
 
-    #-----[ GIT REPOSITORIES ]
-    print(f":: Fetching AlterEGO tools, config and other stuff...")
+    # [ GIT REPOSITORIES ]
+    Msg.console(f":: {_green}Fetching AlterEGO tools, config and other stuff...", wait=5)
 
-    print(f" -> Pulling {gitTOOLS}...")
+    Msg.console(f" -> {_blue}Pulling {gitTOOLS}.", wait=5)
     git(gitTOOLS, localTOOLS)
 
     if mode == 'beast':
         print(f" -> Pulling {gitEGO}")
+        Msg.console(f" -> {_blue}Pulling {gitEGO}.", wait=5)
         git(gitEGO, localEGO)
 
-    #-----[ TIMEZONE & CLOCK ]
+    # [ TIMEZONE & CLOCK ]
+    Msg.console(f":: {_green}Setting clock and timezone...", wait=5)
     os.symlink(f'/usr/share/zoneinfo/{timezone}', '/etc/localtime')
     execute(f'timedatectl set-ntp true')
     execute(f'hwclock --systohc --utc')
 
-    #-----[ LOCALE ]
+    # [ LOCALE ]
 
-    print(':: Generating locale...')
+    Msg.console(f":: {_green}Generating locale...", wait=5)
     execute(f'sed -i "s/#en_US.UTF-8/en_US.UTF-8/" /etc/locale.gen')
     with open('/etc/locale.conf', 'w') as locale_conf:
         locale_conf.write('LANG=en_US.UTF-8')
     os.putenv('LANG', 'en_US.UTF-8')
     execute(f'locale-gen')
 
-    #-----[ NETWORK CONFIGURATION ]
+    # [ NETWORK CONFIGURATION ]
 
-    print(':: Setting up network...')
+    Msg.console(f":: {_green}Setting up network...", wait=5)
     with open('/etc/hostname', 'w') as etc_hostname:
         etc_hostname.write(hostname)
     with open('/etc/hosts', 'w') as etc_hosts:
@@ -400,107 +469,104 @@ def sysconfig(mode):
 127.0.1.1	{hostname}.localdomain	{hostname}
 ''')
 
-    print(' -> Enabling NetworkManager daemon...')
+    Msg.console(f" -> {_blue}Enabling NetworkManager daemon...", wait=2)
     execute(f'systemctl enable NetworkManager.service')
 
-    #-----[ POPULATING /etc/skel ]
+    # [ POPULATING /etc/skel ]
 
     if mode == 'beast':
+        Msg.console(f":: {_green}Populating /etc/skel...", wait=5)
         src = f"{localEGO}/config/"
         dst = f"/etc/skel/"
         copy_recursive(src, dst)
 
-    #-----[ USERS and PASSWORDS ]
+    # [ USERS and PASSWORDS ]
 
-    print(':: Configuring users and passwords...')
-    print(' -> Setting password for root user...')
+    Msg.console(f":: {_green}Configuring users and passwords...", wait=5)
+    Msg.console(f" -> {_blue}Setting password for root user.", wait=2)
     subprocess.run(['passwd'], input=f'{root_passwd}\n{root_passwd}\n', text=True)
 
     if mode == 'beast':
-        print(f' -> Creating user {user}...')
+        Msg.console(f" -> {_blue}Creating user {user}", wait=2)
         execute(f'useradd -m -g users -G wheel {user}') 
-        print(f' -> Setting password for {user}...')
+        Msg.console(f" -> {_blue}Setting password for {user}", wait=2)
         subprocess.run(['passwd', user], input=f'{user_passwd}\n{user_passwd}\n', text=True)
 
-        print(f' -> Enabling sudoers...')
+        Msg.console(f" -> {_blue}Enabling sudoers for {user}", wait=2)
         execute(f'sed -i "s/# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/" /etc/sudoers')
 
-    #-----[ SHARED RESOURCES ]
+    # [ SHARED RESOURCES ]
 
     if mode == 'beast':
+        Msg.console(f":: {_green}Deploying shared resources...", wait=5)
         shared_resources()
         shared_bin()
         shared_wordlist()
         shared_reverse_shell()
-        #### assets
+        # -- assets
         copy_recursive(os.path.join(localEGO, 'share', 'assets'), os.path.join(usr_local, 'share', 'assets'))
-        #### backgrounds
+        # -- backgrounds
         copy_recursive(os.path.join(localEGO, 'share', 'backgrounds'), os.path.join(usr_local, 'share', 'backgrounds'))
 
-    #-----[ SWAPFILE ]
+    # [ SWAPFILE ]
 
-    print(f":: Creating the swapfile...")
     swapfile()
 
-    #-----[ FIX PACMAN MIRRORLIST ]
+    # [ FIX PACMAN MIRRORLIST ]
 
-    # [ PACMAN ]_______________________________________________________________
+    # [ PACMAN ]
 
-    msg(f'just wait', color='green')
-    msg(f'MODE IS {mode}', color='green')
     pacman(mode)
-    msg(f'just wait', color='green')
-    
 
-    # [ YAY ]__________________________________________________________________
+    # [ YAY ]
 
     if mode == 'beast':
-        msg(":: Installing YAY...", color='green')
+        Msg.console(f":: {_green}Installing YAY...", wait=5)
         execute(f"git clone https://aur.archlinux.org/yay.git", cwd='/opt')
         execute(f"chown -R {user}:users /opt/yay")
         execute(f"su {user} -c 'makepkg -si --needed --noconfirm'", cwd='/opt/yay')
 
-        msg(f":: Installing AUR packages...", color='green')
+        Msg.console(f":: {_green}Installing AUR packages...", wait=5)
         pkgs_list = ' '.join(packages('yay'))
-        _yay = execute(f"sudo -u {user} /bin/bash -c 'yay -S --noconfirm {pkgs_list}'")
-        if _yay.returncode == 0:
-            msg('YAY!!', color='green')
+        Msg.console(f" -> {_blue}Will be installed:\n{pkgs_list}", wait=4)
+        execute(f"sudo -u {user} /bin/bash -c 'yay -S --noconfirm {pkgs_list}'")
 
-    # [ SDDM ]_________________________________________________________________
+    # [ SDDM ]
 
     if mode == 'beast':
+        Msg.console(f":: {_green}Deploying sddm and starting the service...", wait=5)
         shutil.copy(os.path.join(localEGO, 'global', 'etc', 'sddm.conf'), '/etc/sddm.conf')
         copy_recursive(os.path.join(localEGO, 'global', 'usr', 'share', 'sddm'), '/usr/share/sddm')
+        execute(f'systemctl enable sddm.service')
 
-    #-----[ GENERATING mandb ]
+    # [ GENERATING mandb ]
 
+    Msg.console(f":: {_green}Generating mandb...", wait=5)
     execute(f"mandb")
 
-    #-----[ SETTING JAVA DEFAULT ]
+    # [ SETTING JAVA DEFAULT ]
 
-        #### Burpsuite
-        #... Not running with java 16.
-        #... will need to install jre11-openjdk.
-        #... $ sudo archlinux-java set java-11-openjdk
+    # -- Burpsuite
+    # .. Not running with java 16.
+    # .. will need to install jre11-openjdk.
+    # .. $ sudo archlinux-java set java-11-openjdk
 
-    execute(f"archlinux-java set java-11-openjdk")
+    if mode == 'beast':
+        Msg.console(f":: {_green}Fixing Java...", wait=2)
+        execute(f"archlinux-java set java-11-openjdk")
 
-    #-----[ BOOTLOADER ]
+    # [ BOOTLOADER ]
 
-    print(':: Installing and configuring the bootloader...')
+    Msg.console(f":: {_green}Installing and configuring the bootloader...", wait=5)
     execute(f'grub-install /dev/sda')
     execute(f'grub-mkconfig -o /boot/grub/grub.cfg')
 
-    #-----[ VIRTUALBOX SERVICES ]
+    # [ VIRTUALBOX SERVICES ]
 
     if mode == 'beast':
+        Msg.console(f":: {_green}Starting vbox service...", wait=5)
         execute(f'systemctl start vboxservice.service')
         execute(f'systemctl enable vboxservice.service')
-
-    #-----[ SDDM SERVICES ]
-
-    if mode == 'beast':
-        execute(f'systemctl enable sddm.service')
     
 def main():
 
@@ -513,7 +579,7 @@ def main():
 
     if args.install:
         mode = args.install
-        print(f":: This will install AlterEGO Linux in {mode} mode...")
+        Msg.console(f":: {_green}This will install AlterEGO Linux in {mode} mode...", wait=10)
         installer(mode)
     if args.sysconfig:
         mode = args.sysconfig
