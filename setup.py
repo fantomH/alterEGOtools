65a73520723da4c378a5ca2faa42dbceecd3154d
Pascal Malouin <pascal.malouin@gmail.com>
2021-09-08 08:22:27 -0400

    Changing alterEGOtools structure.

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='alterEGOtools',
+    version='0.0.1',
+    description='Installation tools for alterEGO Linux.',
+    author='Pascal Malouin',
+    author_email='pascal.malouin@gmail.com',
+    url='https://github.com/fantomH/alterEGOtools',
+    packages=['alterEGOtools'],
+    install_requires=[]
+)
+
